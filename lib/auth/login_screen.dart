0b4ee8f571a4d4b9b07551611482c6eac747439b
adogbeji <baka_1993@yahoo.co.uk>
2023-10-31 21:07:22 +0000

    Add TikTok logo to login screen

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -10,10 +10,14 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen')
-      ),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/tiktok.png'),
+          ]
+        ),
+      )
     );
   }
 }
