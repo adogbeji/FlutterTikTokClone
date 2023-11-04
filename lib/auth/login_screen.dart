cb0b57c12e8e8c722edf40de77ab21dc2abb339c
adogbeji <baka_1993@yahoo.co.uk>
2023-11-04 21:09:05 +0000

    Start applying fonts & add more text to login screen

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,20 +12,39 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-          Image.asset(
-            'assets/images/tiktok.png',
-            width: 200,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Image.asset(
+                'assets/images/tiktok.png',
+                width: 200,
+              ),
+              Text(
+                'Welcome',
+                style: GoogleFonts.acme(
+                  fontSize: 34,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Glad to see you',
+                style: GoogleFonts.acme(
+                  fontSize: 34,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
           ),
-          const Text('Welcome'),
-        ],),
+        ),
       ),
-    ),);
+    );
   }
 }
