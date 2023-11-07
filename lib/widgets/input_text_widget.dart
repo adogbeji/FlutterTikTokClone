647293de8ce465438fdd7aa249a1cf4554c30be7
adogbeji <baka_1993@yahoo.co.uk>
2023-11-07 21:16:00 +0000

    Start adding variables for InputTextWidget

--- lib/widgets/input_text_widget.dart
+++ lib/widgets/input_text_widget.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class InputTextWidget extends StatelessWidget {
-  const InputTextWidget({super.key});
+  // const InputTextWidget({super.key});
+
+  final TextEditingController textEditingController;
+  final IconData? iconData;
+  final String? assetReference;
 
   @override
   Widget build(BuildContext context) {
