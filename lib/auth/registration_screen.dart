9edac2cb85bf403a579436642dfbb501573c5161
adogbeji <baka_1993@yahoo.co.uk>
2024-03-11 20:40:20 +0000

    Add name, email & password controllers to register screen

--- lib/auth/registration_screen.dart
+++ lib/auth/registration_screen.dart
@@ -8,6 +8,11 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  // Name, Email & Password Controllers
+  TextEditingController nameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
